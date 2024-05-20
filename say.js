4d401e746adf5e9445ef29d0ade1428db8108dac
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-05-20 16:27:06 +0100

    .

--- say.js
+++ say.js
@@ -1,6 +1,6 @@
-function say() {
+function say() { //name of the function is say
     console.log("output from say()");
 };
 console.log("Dibiaezue");
-say();
+say();// the parentheses enables the function say to be called
 console.log("Obiajulu");
