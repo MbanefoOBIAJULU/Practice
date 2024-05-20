f1a49a45dc9f7172026fa0616300bc01fd6aba59
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-05-20 16:51:23 +0100

    .

---
+++ say2.js
@@ -0,0 +1,13 @@
+console.log("hello");
+console.log("hi");
+console.log("how do you do");
+console.log("Quite all right");// we can use the call function to call everything once
+
+function say(text) {
+    console.log(text);
+  }
+  say("hello");
+  say("hi");
+  say("how do you do");
+  say("Quite all right");
+  
