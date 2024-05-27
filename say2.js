3543411787deecb5dec52466515b3445c144dacd
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-05-27 09:41:34 +0100

    .

--- say2.js
+++ say2.js
@@ -3,11 +3,21 @@ console.log("hi");
 console.log("how do you do");
 console.log("Quite all right");// we can use the call function to call everything once
 
-function say(text) {
+/*function say(text) {
     console.log(text);
   }
   say("hello");
   say("hi");
   say("how do you do");
   say("Quite all right");// the function name say helped to call without repeating console.log
+
+  function say(text) {
+    console.log("==> " + text);
+  }
+  
+  say("hello");       // => hello
+  say("hi");          // => hi
+  say("how are you"); // => how are you
+  say("I'm fine");    // => I'm fine*/
+  
   
