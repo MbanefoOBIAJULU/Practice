8a6ed82b00c2e6dec922fe2ab7c934b2ad135421
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-05-27 09:30:41 +0100

    .

--- say.js
+++ say.js
@@ -1,6 +1,26 @@
-function say() { //name of the function is say
+/*function say() { //name of the function is say
     console.log("output from say()");
 };
 console.log("Dibiaezue");
 say();// the parentheses enables the function say to be called
 console.log("Obiajulu");
+
+function times(num1, num2) {
+    return num1 * num2;
+  }
+  console.log(times(add(20, 45), subtract(80, 10))); // => 4550
+  // 4550 == ((20 + 45) * (80 - 10))
+  
+  let sum = add(20, 45);
+let difference = subtract(80, 10);
+let result = times(sum, difference);
+console.log(result);*/
+
+function add(left, right) { // left & right are parameters here
+    let sum = left + right;   // left & right are arguments here
+    return sum;
+  }
+  
+  let sum = add(3, 6); // 3 and 6 are arguments
+  console.log(add(3,6,5,7))
+  
